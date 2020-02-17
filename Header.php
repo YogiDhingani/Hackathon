8ab1a134ffe7cb766b53dc9a244ba9da93537295
narendra1520 <46049530+narendra1520@users.noreply.github.com>
2020-02-17 20:08:28 +0530

    common header footer

--- Header.php
+++ Header.php
@@ -1,74 +1,76 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
-  <meta charset="utf-8">
-  <title>Complaint Management System</title>
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <meta content="" name="keywords">
-  <meta content="" name="description">
+<meta charset="utf-8">
+<title>Complaint Management System</title>
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+<meta content="" name="keywords">
+<meta content="" name="description">
 
-  <!-- Favicons -->
-  <link href="img/favicon.png" rel="icon">
-  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+<!-- Favicons -->
+<link href="img/favicon.png" rel="icon">
+<link href="img/apple-touch-icon.png" rel="apple-touch-icon">
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
 
-  <!-- Bootstrap CSS File -->
-  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<!-- Bootstrap CSS File -->
+<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- Libraries CSS Files -->
-  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-  <link href="lib/animate/animate.min.css" rel="stylesheet">
-  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
-  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+<!-- Libraries CSS Files -->
+<link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<link href="lib/animate/animate.min.css" rel="stylesheet">
+<link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+<link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+<link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
 
-  <!-- Main Stylesheet File -->
-  <link href="css/style.css" rel="stylesheet">
+<!-- Main Stylesheet File -->
+<link href="css/style.css" rel="stylesheet">
 
-  <!-- =======================================================
-    Theme Name: NewBiz
-    Theme URL: https://bootstrapmade.com/newbiz-bootstrap-business-template/
-    Author: BootstrapMade.com
-    License: https://bootstrapmade.com/license/
-  ======================================================= -->
+<script type="text/javascript">
+function removeIncl(){
+  var el = document.getElementById("header");
+  el.parentNode.removeChild(el);
+  var el2 = document.getElementsByClassName("mobile-nav-toggle");
+  el2[0].parentNode.removeChild(el2[0]);
+}
+</script>
+
+<!-- =======================================================
+  Theme Name: NewBiz
+  Theme URL: https://bootstrapmade.com/newbiz-bootstrap-business-template/
+  Author: BootstrapMade.com
+  License: https://bootstrapmade.com/license/
+======================================================= -->
 </head>
-
 <body>
-    <button type="button" class="mobile-nav-toggle d-lg-none"><i class="fa fa-bars"></i></button>
+  <!-- <button type="button" class="mobile-nav-toggle d-lg-none">
+    <i class="fa fa-bars"></i>
+  </button> -->
   <!--==========================
   Header
   ============================-->
   <header id="header" class="fixed-top">
-    <div class="container">
-
-      <div class="logo float-left">
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <h1 class="text-light"><a href="#header"><span>NewBiz</span></a></h1> -->
-        <a href="#intro" class="scrollto"><img src="img/logo.png" alt="" class="img-fluid"></a>
-      </div>
-
-      <nav class="main-nav float-right d-none d-lg-block">
-        <ul>
-          <li class="active"><a href="#intro">Home</a></li>
-          <li class="drop-down"><a href="">Compliant</a>
-            <ul>
-              <li><a href="#">New Complaint</a></li>
-              <li><a href="#">Solved Complaint</a></li>
-              <li><a href="#">Pending Complaint</a></li>
-            </ul>
-          </li>
-		  <li><a href="#profile">Profile</a></li>
-		  <li><a href="#team">Team</a></li>
-          <li><a href="#contact">Contact Us</a></li>
-		  <li><a href="#about">About Us</a></li>
-		  <li><a href="#">Log Out</a></li>
-        </ul>
-      </nav><!-- .main-nav -->
-	  
-      
+  <div class="container">
+    <div class="logo float-left">
+      <a href="#intro" class="scrollto"><img src="img/logo.png" alt="" class="img-fluid"></a>
     </div>
-  </header><!-- #header -->
+
+    <nav class="main-nav float-right d-none d-lg-block">
+      <ul>
+        <li class="active"><a href="#intro">Home</a></li>
+        <li class="drop-down"><a href="">Compliant</a>
+          <ul>
+            <li><a href="new_compliant.php">New Complaint</a></li>
+            <li><a href="#">Solved Complaint</a></li>
+            <li><a href="#">Pending Complaint</a></li>
+          </ul>
+        </li>
+      	  <li><a href="#profile">Profile</a></li>
+      	  <li><a href="#team">Team</a></li>
+          <li><a href="#contact">Contact Us</a></li>
+      	  <li><a href="#about">About Us</a></li>
+      	  <li><a href="#">Log Out</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
 </body>
-</html>
--- new_compliant.php
+++ new_compliant.php
@@ -1,119 +1,62 @@
 <html>
-<head>
+<!-- <head>
   <link href="css/style.css" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
-<title>Complaint management System</title>
+<title>Complaint management System</title>-->
 <body>
-  <div style="height:15%">
-  <?php  include 'Header.php';?>
-</div>
-<section id="form" class="clearfix">
-  <div class="container">
-    <div class="intro-img">
-<form>
-  <div class="form-group row">
-    <label for="inputTitle" class="col-sm-2 col-form-label">Title</label>
-    <div class="col-sm-4">
-      <input type="email" class="form-control" id="inputTitle" placeholder="Title">
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="inputCategory" class="col-sm-2 col-form-label">Category</label>
-    <div class="col-sm-4">
-      <input type="email" class="form-control" id="inputCategory" placeholder="Category">
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="inputSubCategory" class="col-sm-2 col-form-label">Sub Category</label>
-    <div class="col-sm-4">
-      <input type="email" class="form-control" id="inputSubCategory" placeholder="Sub Category">
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="inputDescription" class="col-sm-2 col-form-label">Description</label>
-    <div class="col-sm-4">
-      <textarea type="email" rows="5" class="form-control" id="inputDescription" placeholder="Description"></textarea>
-    </div>
-  </div>
-    <div class="form-group row">
-    <label for="inputLocation" class="col-sm-2 col-form-label">Location</label>
-    <div class="col-sm-4">
-      <input type="email" class="form-control" id="inputLocation" placeholder="Location">
-    </div>
-  </div>
-   <div class="form-group row">
-      <label class="col-sm-2 col-form-label" for="exampleInputFile">Upload File</label>
-      <div class="col-sm-4">
-      <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
-   </div>
-    </div>
-  <div class="form-group row">
-    <div class="col-sm-10">
-      <button style="background:#00428a" type="submit" class="btn btn-primary">Complaint</button>
-    </div>
-  </div>
-</form>
-</div>
-</div>
-</section>
-</body>
-
-<footer id="footer">
-  <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong>NewBiz</strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        <!--
-          All the links in the footer should remain intact.
-          You can delete the links only if you purchased the pro version.
-          Licensing information: https://bootstrapmade.com/license/
-          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=NewBiz
-        -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+  <?php include ("header.php");?>
+  <div style="height:12%"></div>
+  <section id="form" class="clearfix">
+    <div class="container">
+      <div class="intro-img">
+        <form>
+          <div class="form-group row">
+            <label for="inputTitle" class="col-sm-2 col-form-label">Title</label>
+            <div class="col-sm-4">
+              <input type="email" class="form-control" id="inputTitle" placeholder="Title">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputCategory" class="col-sm-2 col-form-label">Category</label>
+            <div class="col-sm-4">
+              <input type="email" class="form-control" id="inputCategory" placeholder="Category">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputSubCategory" class="col-sm-2 col-form-label">Sub Category</label>
+            <div class="col-sm-4">
+              <input type="email" class="form-control" id="inputSubCategory" placeholder="Sub Category">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputDescription" class="col-sm-2 col-form-label">Description</label>
+            <div class="col-sm-4">
+              <textarea type="email" rows="5" class="form-control" id="inputDescription" placeholder="Description"></textarea>
+            </div>
+          </div>
+            <div class="form-group row">
+            <label for="inputLocation" class="col-sm-2 col-form-label">Location</label>
+            <div class="col-sm-4">
+              <input type="email" class="form-control" id="inputLocation" placeholder="Location">
+            </div>
+          </div>
+           <div class="form-group row">
+              <label class="col-sm-2 col-form-label" for="exampleInputFile">Upload File</label>
+              <div class="col-sm-4">
+              <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
+           </div>
+            </div>
+          <div class="form-group row">
+            <div class="col-sm-10">
+              <button style="background:#00428a" type="submit" class="btn btn-primary">Complaint</button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
-  </footer><!-- #footer -->
-
-  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
-  <!-- Uncomment below i you want to use a preloader -->
-  <!-- <div id="preloader"></div> -->
-
-  <!-- JavaScript Libraries -->
-  <script src="lib/jquery/jquery.min.js"></script>
-  <script src="lib/jquery/jquery-migrate.min.js"></script>
-  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="lib/easing/easing.min.js"></script>
-  <script src="lib/mobile-nav/mobile-nav.js"></script>
-  <script src="lib/wow/wow.min.js"></script>
-  <script src="lib/waypoints/waypoints.min.js"></script>
-  <script src="lib/counterup/counterup.min.js"></script>
-  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-  <script src="lib/isotope/isotope.pkgd.min.js"></script>
-  <script src="lib/lightbox/js/lightbox.min.js"></script>
-  <nav class="mobile-nav d-lg-none">
-        <ul>
-          <li class=""><a href="#intro">Home</a></li>
-          <li class="drop-down"><a href="">Compliant</a>
-            <ul>
-              <li><a href="#">New Complaint</a></li>
-              <li><a href="#">Solved Complaint</a></li>
-              <li><a href="#">Pending Complaint</a></li>
-            </ul>
-          </li>
-      <li><a href="#profile">Profile</a></li>
-      <li><a href="#team">Team</a></li>
-          <li class=""><a href="#contact">Contact Us</a></li>
-      <li class="active"><a href="#about">About Us</a></li>
-      <li><a href="#">Log Out</a></li>
-        </ul>
-      </nav>
-  <!-- Contact Form JavaScript File -->
-  <script src="contactform/contactform.js"></script>
-
-  <!-- Template Main Javascript File -->
-  <script src="js/main.js"></script>
-
+  </section>
+  <?php include("footer.php");?>
+  </body>
 </html>
