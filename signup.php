a638d6b60a616bcc36d849f78456a9d66b621e37
Rohan <rohanpatel1309@gmail.com>
2020-02-23 00:01:06 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -14,23 +14,23 @@ label{
   <section id="intro" class="clearfix">
     <div class="container">
       <div class="intro-info">
-        <form>
+        <form method="post">
           <div class="form-group row">
             <label for="inputName" class="col-sm-2 col-form-label">Name</label>
             <div class="col-sm-4">
-              <input type="text" class="form-control" id="inputName" placeholder="Name">
+              <input type="text" class="form-control" id="inputName" placeholder="Name" name="nm">
             </div>
           </div>
           <div class="form-group row">
             <label for="inputPhone" class="col-sm-2 col-form-label">Phone No.</label>
             <div class="col-sm-4">
-              <input type="tel" class="form-control" id="inputPhone" placeholder="Phone No.">
+              <input type="tel" class="form-control" id="inputPhone" placeholder="Phone No." name="phone_no">
             </div>
           </div>
           <div class="form-group row">
             <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
             <div class="col-sm-4">
-              <input type="email" class="form-control" id="inputEmail" placeholder="example@gmail.com">
+              <input type="email" class="form-control" id="inputEmail" placeholder="example@gmail.com" name="eid">
             </div>
           </div>
           <div class="form-group row">
@@ -49,7 +49,7 @@ label{
           <div class="form-group row">
             <label for="inputPassword" class="col-sm-2 col-form-label">New Password</label>
             <div class="col-sm-4">
-              <input type="password" class="form-control" id="inputPassword" placeholder="New Password">
+              <input type="password" class="form-control" id="inputPassword" placeholder="New Password" name="password">
             </div>
           </div>
           <div class="form-group row">
@@ -60,7 +60,8 @@ label{
           </div>
           <div class="form-group row">
             <div class="col-sm-10">
-              <a href="#" class="btn-get-started scrollto">Register</a>
+              <!-- <a href="#" class="btn-get-started scrollto">Register</a>-->
+			   <input type="submit" class="btn-get-started scrollto" value="Register" name="register">
               <a href="#" class="btn-services scrollto" onclick="window.history.back()">Back to Login</a>
             </div>
           </div>
@@ -68,6 +69,15 @@ label{
       </div>
     </div>
   </section>
-  <?php include("footer.php");?>
+  <?php include("footer.php");
+		if(isset($_POST['register'])){
+			$name=$_POST['nm'];
+			$eid=$_POST['eid'];
+			$phone_no=$_POST['phone_no'];
+			
+			
+			
+		}
+  ?>
 </body>
 </html>
