2b106fce6a09c084e9899f2ae8cc47b0c1d1e1e0
sunilchauhan940 <sunilchauhan940@users.noreply.github.com>
2020-02-26 21:43:22 +0530

    Admin Panel

---
+++ Admin/userdelete.php
@@ -0,0 +1,13 @@
+<?php include 'config2.php';
+    $id=$_REQUEST['id']; 
+    $delete="DELETE FROM user WHERE user_id='$id'";
+    if(mysqli_query($conn, $delete))
+    {
+      echo "Records deleted successfully.";
+    }
+    else
+    {
+      echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+    }
+    header('Location:viewuser.php'); 
+?>
